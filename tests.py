9bbee39e8fad6a46a0784ece341237742fbf2395
Kv1lon <skrypnyk.michael@gmail.com>
2024-09-10 18:48:29 +0300

    tests improved

--- tests.py
+++ tests.py
@@ -1,28 +1,36 @@
 import unittest
-from selenium_scraper import LUNRentScraperSelenium
+from retrying import retry
 
+class TestLUNRentScraperReal(unittest.TestCase):
+    def setUp(self):
+        self.url = 'https://lun.ua/uk/search?currency=UAH&geo_id=10009580&has_eoselia=false&is_without_fee=false&price_sqm_currency=UAH&section_id=2&sort=insert_time'
 
-class TestLUNRentScraperSeleniumReal(unittest.TestCase):
-
-    def test_scrape_real_page(self):
-        url = 'https://lun.ua/uk/search?currency=UAH&geo_id=10009580&has_eoselia=false&is_without_fee=false&price_sqm_currency=UAH&section_id=2&sort=insert_time'
-
-        scraper = LUNRentScraperSelenium(url, last_scraped_id=-1)
-
+    def validate_scraper_results(self, url, lun_scraper):
+        scraper = lun_scraper(url, last_scraped_id=-1)
         results = scraper.scrape()
 
         self.assertGreater(len(results), 0, "No realties were scraped from the real page.")
 
         first_result = results[0]
 
-        self.assertIn('id', first_result)
-        self.assertIn('price', first_result)
-        self.assertIn('address', first_result)
-        self.assertIn('description', first_result)
-        self.assertIn('picture', first_result)
+        required_fields = ['id', 'price', 'address', 'description', 'picture']
+        for field in required_fields:
+            self.assertIn(field, first_result)
+
 
         print(first_result)
 
+
+    @retry(wait_fixed=2000, stop_max_attempt_number=3)
+    def test_scrape_selenium(self):
+        from selenium_scraper import LUNRentScraperSelenium
+        self.validate_scraper_results(self.url, LUNRentScraperSelenium)
+
+
+    @retry(wait_fixed=2000, stop_max_attempt_number=3)
+    def test_scrape_curl_cffi(self):
+        from scraper import LUNRentScraper
+        self.validate_scraper_results(self.url, LUNRentScraper)
 
 if __name__ == '__main__':
     unittest.main()
