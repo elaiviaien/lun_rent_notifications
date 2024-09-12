2dd0abb314643c3d5d36f3689496cb56f5371b16
Kv1lon <skrypnyk.michael@gmail.com>
2024-09-12 16:28:36 +0300

    fix orders.csv path

--- core/utils.py
+++ core/utils.py
@@ -3,14 +3,14 @@ import tempfile
 
 
 def get_orders() -> list[list[str]]:
-    with open("../orders.csv", "r", encoding="utf-8") as file:
+    with open("orders.csv", "r", encoding="utf-8") as file:
         reader = csv.DictReader(file)
         orders = [list(order.values()) for order in reader]
     return orders
 
 
 def save_order(user_id: int, search_url: str, last_scraped_id: int) -> None:
-    with open("../orders.csv", "r", encoding="utf-8") as file:
+    with open("orders.csv", "r", encoding="utf-8") as file:
         reader = csv.reader(file)
         orders = list(reader)
         user_orders = [order for order in orders if order[0] == str(user_id)]
@@ -21,18 +21,18 @@ def save_order(user_id: int, search_url: str, last_scraped_id: int) -> None:
         else:
             orders.append([user_id, search_url, last_scraped_id])
 
-    with open("../orders.csv", "w", newline="", encoding="utf-8") as file:
+    with open("orders.csv", "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerows(orders)
 
 
 def remove_order(user_id: int) -> None:
-    with open("../orders.csv", "r", encoding="utf-8") as file:
+    with open("orders.csv", "r", encoding="utf-8") as file:
         reader = csv.reader(file)
         orders = list(reader)
         orders = [order for order in orders if order[0] != str(user_id)]
 
-    with open("../orders.csv", "w", newline="", encoding="utf-8") as file:
+    with open("orders.csv", "w", newline="", encoding="utf-8") as file:
         writer = csv.writer(file)
         writer.writerows(orders)
 
