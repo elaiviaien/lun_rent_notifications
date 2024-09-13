712800b59d4fe9b004c40879051dbbe036e5e926
Kv1lon <skrypnyk.michael@gmail.com>
2024-09-13 15:32:47 +0300

    huey -> celery
    curl_cffi couldn't work properly with huey. Too frequent network errors

--- core/tasks.py
+++ core/tasks.py
@@ -1,12 +1,16 @@
-from huey import RedisHuey, crontab
-
 from bot import process_order, send_notifications
 from .utils import get_orders, save_order
 
-huey = RedisHuey("LUN_notifications", host="redis", port=6379)
+from celery import Celery
+from celery.schedules import crontab
 
+app = Celery('tasks', broker='redis://redis:6379/0', backend='redis://redis:6379/0')
 
-@huey.periodic_task(crontab(minute="*/5"))
+app.conf.update(
+    result_expires=3600,
+)
+
+@app.task
 def periodic_scraping():
     orders = get_orders()
     for order in orders:
@@ -15,3 +19,10 @@ def periodic_scraping():
         if realties:
             save_order(int(order[0]), order[1], int(realties[-1]["id"]))
             send_notifications(order[0], realties)
+
+app.conf.beat_schedule = {
+    'periodic-scraping-every-5-minutes': {
+        'task': 'core.tasks.periodic_scraping',
+        'schedule': crontab(minute='*/5'),
+    },
+}
